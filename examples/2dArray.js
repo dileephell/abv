1c44f251474622b0212092806d7264f2037d05f2
Dileep Singh <dileep2008apex@gmail.com>
2017-08-10 15:46:19 +0530

    Update 2dArray.js

--- examples/2dArray.js
+++ examples/2dArray.js
@@ -9,3 +9,26 @@ console.log(items[1][0]);
 console.log(items[1][1]);
 
 console.log(items);
+
+
+/* Create n-dimensional array */
+
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
+createArray();     // [] or new Array()
+
+createArray(2);    // new Array(2)
+
+createArray(3, 2); // [new Array(2),
+                   //  new Array(2),
+                   //  new Array(2)]
