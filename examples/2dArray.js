63a52aa39ffd87403108f1a5ae54f68e487c28df
Dileep Singh <dileep2008apex@gmail.com>
2017-08-10 15:34:10 +0530

    Create 2dArray.js

---
+++ examples/2dArray.js
@@ -0,0 +1,11 @@
+var items = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+console.log(items[0][0]); 
+console.log(items[0][1]);
+console.log(items[1][0]);
+console.log(items[1][1]);
+
+console.log(items);
