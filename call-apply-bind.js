bd34830aae48be810d9385ad954d961934efaeb5
Dileep Singh <dileep2008apex@gmail.com>
2017-08-16 15:59:14 +0530

    Update call-apply-bind.js

--- call-apply-bind.js
+++ call-apply-bind.js
@@ -57,3 +57,51 @@ Function.prototype.bind = function(ctx) {
         fn.apply(ctx, arguments);
     };
 };
+
+
+
+
+/* another implementation of CALL, APPLY, BIND */
+
+They all attach this into function (or object) and the difference is in the function invocation (see below).
+
+call attaches this into function and executes the function immediately:
+
+var person = {  
+  name: "James Smith",
+  hello: function(thing) {
+    console.log(this.name + " says hello " + thing);
+  }
+}
+
+person.hello("world");  // output: "James Smith says hello world"
+person.hello.call({ name: "Jim Smith" }, "world"); // output: "Jim Smith says hello world"
+bind attaches this into function and it needs to be invoked separately like this:
+
+var person = {  
+  name: "James Smith",
+  hello: function(thing) {
+    console.log(this.name + " says hello " + thing);
+  }
+}
+
+person.hello("world");  // output: "James Smith says hello world"
+var helloFunc = person.hello.bind({ name: "Jim Smith" });
+helloFunc("world");  // output: Jim Smith says hello world"
+or like this:
+
+...    
+var helloFunc = person.hello.bind({ name: "Jim Smith" }, "world");
+helloFunc();  // output: Jim Smith says hello world"
+apply is similar to call except that it takes an array-like object instead of listing the arguments out one at a time:
+
+function personContainer() {
+  var person = {  
+     name: "James Smith",
+     hello: function() {
+       console.log(this.name + " says hello " + arguments[1]);
+     }
+  }
+  person.hello.apply(person, arguments);
+}
+personContainer("world", "mars"); // output: "James Smith says hello mars", note: arguments[0] = "world" , arguments[1] = "mars"                                     
